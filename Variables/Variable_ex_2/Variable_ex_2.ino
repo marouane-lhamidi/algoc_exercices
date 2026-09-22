ef9e69f586d8b0b683f6e14adaea75792d18fbef
Marouane_lhamidi <marouane@192.168.11.159>
2026-09-22 14:49:02 +0000

    adding varaible exercice number 2

---
+++ Variables/Variable_ex_2/Variable_ex_2.ino
@@ -0,0 +1,36 @@
+#include <algoC.h>
+
+void application(ALGOC_APP)
+{
+    // The player starts with 10 points
+    int points = 10;
+
+    writeLine(ALGOC, points);
+
+
+    // TASK 1:
+    // Change points to 15
+
+
+
+    // Display the new value
+    writeLine(ALGOC, points);
+
+
+    // TASK 2:
+    // Add 5 points to the CURRENT value
+
+
+
+    // Display the new value
+    writeLine(ALGOC, points);
+
+
+    // TASK 3:
+    // Remove 3 points from the CURRENT value
+
+
+
+    // Display the final value
+    writeLine(ALGOC, points);
+}
