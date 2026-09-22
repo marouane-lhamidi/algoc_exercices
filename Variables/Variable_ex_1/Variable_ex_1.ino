a71e02f892b944dcc113308dcfb45be858bcaa6e
Marouane_lhamidi <marouane@192.168.11.159>
2026-09-22 14:35:38 +0000

    first commit

---
+++ Variables/Variable_ex_1/Variable_ex_1.ino
@@ -0,0 +1,9 @@
+#include <algoC.h>
+
+void application(ALGOC_APP)
+{
+    // Create a variable called age
+    // Give it your age
+    // Display it
+    // To Display Variable You Need to use writeLine(ALGOC, age);
+}
